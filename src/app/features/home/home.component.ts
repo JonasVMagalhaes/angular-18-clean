bd94c1439205b5bf28a05d0fc44572918eaa0bed
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-09-21 23:48:41 -0300

    First commit

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { RouteEnum } from '@enums/routes/route.enum';
+import {ScheduleComponent} from "./components/calendar/schedule.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
+  imports: [
+    ScheduleComponent
+  ],
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
