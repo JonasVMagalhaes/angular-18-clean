f9c18afdae7a1a4c9452b49a401778b02a40299a
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-10-13 13:10:52 -0300

    Adicionada condicional para executar somente o adapter module no modulo de desenvolvimento

--- src/app/core/adapters/http-client/http-client-adapter.ts
+++ src/app/core/adapters/http-client/http-client-adapter.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientMock } from '@assets/mocks/http-client-mock';
+import {environment} from "../../../../environments/environment";
+
 @NgModule({
   declarations: [],
   imports: [],
   providers: [
     {
       provide: HttpClient,
-      useClass: HttpClientMock
+      useClass: environment.production ? HttpClient : HttpClientMock
     },
   ]
 })
