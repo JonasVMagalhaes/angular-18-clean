0348fa9b2a1ed876abc355a99a94ac346d11f404
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-10-14 19:17:45 -0300

    Ajuste de componentes de hint error

--- src/app/shared/components/hint-error/hint-error.component.ts
+++ src/app/shared/components/hint-error/hint-error.component.ts
@@ -2,12 +2,17 @@ import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { InfoHintError } from './models/info-hint-error/info-hint-error.interface';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-hint-error',
   templateUrl: './hint-error.component.html',
   standalone: true,
   styleUrls: ['./hint-error.component.scss'],
+  imports: [
+    NgIf,
+    NgForOf
+  ]
 })
 export class HintErrorComponent {
   @Input({ required: true }) id: string;
